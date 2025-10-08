e91c6066c64c567e0b050ad392a3b5186cd8fc13
mamad <rasti20081387@gmail.com>
2025-10-09 00:55:49 +0330

    django 2

--- accounts_app/urls.py
+++ accounts_app/urls.py
@@ -2,8 +2,5 @@ from django.urls import path
 from . import views 
 
 urlpatterns = [
-    path('mamad', views.mamad),
-    path('milad', views.milad),
-    path('mehdi', views.mehdi),
-    path('amin', views.amin),
+    path('<username>', views.profile)
 ]
