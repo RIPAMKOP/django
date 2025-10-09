39ff9d45717da4e0dcb92aac2e984ea867f1c644
mamad <rasti20081387@gmail.com>
2025-10-10 01:32:12 +0330

    django

--- accounts_app/views.py
+++ accounts_app/views.py
@@ -1,9 +1,8 @@
 from django.shortcuts import render,HttpResponse
-# Create your views here.
+
 
 def profile(request , username) :
-    return HttpResponse(f'<h1> Hello {username} </h1>')
-    
+    return render(request , "accounts_app/profile.html")
 
 
 def info(request):
