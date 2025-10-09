fa583c6762cbc00509fec52acc4913b8aebd419c
mamad <rasti20081387@gmail.com>
2025-10-09 14:25:54 +0330

    django

--- accounts_app/views.py
+++ accounts_app/views.py
@@ -1,17 +1,9 @@
 from django.shortcuts import render,HttpResponse
 # Create your views here.
-users = ['amir', 'milad' , 'hoseein' , 'ali', 'farid', 'reza']
-blocked_users = ['milad','ali']
 
-
-def profile(request , username):
-    if username in users: 
-             if username in blocked_users: 
-                 return HttpResponse(f'<h1> {username} is blocked <h1>')
-             else:
-                     return HttpResponse(f"<h1 style='color:red'> This Is {username} profile <h1>")
-    else:       
-                   return HttpResponse('This Username is not registered')
+def profile(request , username) :
+    return HttpResponse(f'<h1> Hello {username} </h1>')
+    
 
 
 def info(request):
