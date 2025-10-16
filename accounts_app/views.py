473f97f6c00ec2d568e337e568fb85461260a1b7
mamad <rasti20081387@gmail.com>
2025-10-17 02:41:55 +0330

    django

--- accounts_app/views.py
+++ accounts_app/views.py
@@ -1,9 +1,37 @@
 from django.shortcuts import render,HttpResponse
+from django.http import Http404
 
+users = [
+    {
+        'username' : 'amir' , 
+        'name' : 'amirhossein', 
+        'lastname' : 'amir' , 
+        'phone' : '09368432094' ,
+        'city' : 'iran'
+    },
+    {
+        'username' : 'milad80' , 
+        'name' : 'milad', 
+        'lastname' : 'dehyami' , 
+        'phone' : '09368432094' ,
+        'city' : 'esf'
+    },
+    {
+        'username' : 'hossein90' , 
+        'name' : 'hossein', 
+        'lastname' : 'rasti' , 
+        'phone' : '09368432094' ,
+        'city' : 'ysj'
+    },
+]
 
 def profile(request , username) :
-    
-    return render(request , "accounts_app/profile.html", context={"name": username})
+    for user in users : 
+        if user['username'] == username:
+            return render(request, 'accounts_app/profile.html', context={'user': user})
+        
+        return Http404('This user in not exist')
+   
 
 
 def info(request):
