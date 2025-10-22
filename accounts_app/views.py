9498dc62d8bd22f61e30155517b2f34388bbf3db
mamad <rasti20081387@gmail.com>
2025-10-23 02:21:57 +0330

    django

--- accounts_app/views.py
+++ accounts_app/views.py
@@ -1,38 +1,36 @@
-from django.shortcuts import render,HttpResponse
+from django.shortcuts import render, HttpResponse
 from django.http import Http404
 
 users = [
     {
-        'username' : 'amir' , 
-        'name' : 'amirhossein', 
-        'lastname' : 'amir' , 
-        'phone' : '09368432094' ,
-        'city' : 'iran'
+        'username': 'amir',
+        'name': 'amirhossein',
+        'lastname': 'amiri',
+        'phone': '09368432094',
+        'city': 'iran',
     },
     {
-        'username' : 'milad80' , 
-        'name' : 'milad', 
-        'lastname' : 'dehyami' , 
-        'phone' : '09368432094' ,
-        'city' : 'esf'
+        'username': 'milad',
+        'name': 'milad',
+        'lastname': 'dehyami',
+        'phone': '09368432094',
+        'city': 'esf',
     },
     {
-        'username' : 'hossein90' , 
-        'name' : 'hossein', 
-        'lastname' : 'rasti' , 
-        'phone' : '09368432094' ,
-        'city' : 'ysj'
-    },
+        'username': 'hossein',
+        'name': 'hossein',
+        'lastname': 'rasti',
+        'phone': '09368432094',
+        'city': 'ysj',
+    }
 ]
 
-def profile(request , username) :
-    for user in users : 
+def profile(request, username):
+    for user in users:
         if user['username'] == username:
             return render(request, 'accounts_app/profile.html', context={'user': user})
-        
-        return Http404('This user in not exist')
-   
+    raise Http404('This user does not exist')
 
 
 def info(request):
-    return render(request , "accounts_app/info .html")
+    return render(request, "accounts_app/info.html")
