4cabe44204924d0a1c958777994bbf1437aa530f
mamad <rasti20081387@gmail.com>
2025-11-26 01:56:18 +0330

    django

--- shop/urls.py
+++ shop/urls.py
@@ -5,5 +5,6 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('home_app.urls')),
-    path('account/', include('accounts_app.urls') )
+    path('account/', include('accounts_app.urls') ),
+    path('course/', include('courses_app.urls')),
 ]
